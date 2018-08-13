699c1e17f3dac4c621d7eafcebdeec99d4ac7d54
birozso <birozso@gmail.com>
2018-08-13 15:57:49 +0200

    initial creation

---
+++ src/components/WindmillStyle.js
@@ -0,0 +1,73 @@
+const K_CIRCLE_SIZE = 45;
+const K_STICK_SIZE = 15;
+const K_STICK_WIDTH = 35;
+
+
+const windmillStyle = {
+  // initially any map object has left top corner at lat lng coordinates
+
+  position: 'absolute',
+  width: K_CIRCLE_SIZE,
+  height: K_CIRCLE_SIZE + K_STICK_SIZE,
+  left: -K_CIRCLE_SIZE *2,
+  top: -(K_CIRCLE_SIZE + K_STICK_SIZE)
+
+};
+
+const windmillCircleStyle = {
+  position: 'absolute',
+  left: 50,
+  top: 10,
+  width: K_CIRCLE_SIZE,
+  height: K_CIRCLE_SIZE,
+  textAlign: 'center',
+  color: 'white',
+  fontSize: 0,
+  fontWeight: 'bold',
+  padding: 0,
+  cursor: 'pointer'
+
+};
+
+
+const windmillCircleStyleHover = {
+  position: 'absolute',
+  left: 50,
+  top: 10,
+  width: K_CIRCLE_SIZE*1.8,
+  height: K_CIRCLE_SIZE*1.8,
+  textAlign: 'center',
+  color: '#3f51b5',
+  fontSize: 16,
+  fontWeight: 'bold',
+  padding: 0,
+  cursor: 'pointer',
+  border: '4px solid #3f51b5',
+  borderRadius: '16px',
+  color: '#f44336'
+
+};
+
+
+
+
+const windmillStickStyle = {
+  position: 'absolute',
+  left: K_CIRCLE_SIZE ,
+  top: K_CIRCLE_SIZE,
+  width: K_STICK_WIDTH,
+  height: K_STICK_SIZE
+
+};
+
+const windmillStickStyleHover = {
+  ...windmillStickStyle
+
+};
+
+
+export {
+  windmillStyle,
+  windmillCircleStyle, windmillCircleStyleHover,
+  windmillStickStyle, windmillStickStyleHover,
+  K_CIRCLE_SIZE, K_STICK_SIZE};
