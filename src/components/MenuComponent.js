3e44218a19b9038bc6870d7a709bfd183a0506cf
birozso <birozso@gmail.com>
2018-08-20 11:35:42 +0200

    wrapper, query function and infowindow added

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,59 +1,56 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types'
 import '../App.css';
-import escapeRegExp from 'escape-string-regexp'
-import sortBy from 'sort-by'
-import MyMapComponent from '../MyMapComponent.js'
+import InfoWindowComponent from './InfoWindowComponent';
+
 
 class MenuComponent extends Component {
 
     static propTypes = {
         searched: PropTypes.array.isRequired,
-        updateSearched: PropTypes.func
+
       }
 
-    state = {
-        showSearch: null,
-        searchedLocation: null
-    }
 
 
-    // blocking the first space in the search field by replace method && update query
 
-    searcher = (query) => {
+    // blocking the first space in the search field by replace method && update query in App
+
+    unifier = (query) => {
         let trimmedQuery = query.replace(/^\s+/, '');
         this.props.updateQuery(trimmedQuery)
-        const showSearch = [];
-
-
-}
+    }
 
 
 
     render() {
+                const {needOpen, venueInfo} = this.props
+// access the App Component and modify the query state value and filter on it
 
-
-        if (this.props.query) {
-          console.log(this.props.query)
-          const match = new RegExp(escapeRegExp(this.props.query), 'i')
-          this.showSearch = this.props.searched.filter((sear) => match.test(sear.title))
-        } else {
-            this.showSearch = this.props.searched
-        }
-        this.showSearch.sort(sortBy('title'));
 
 
 
 
         return(
 
-            <div id = "sidebar" className = "sidebar" role = "search" >
-                <a className = "hamburger-big-menu" tabIndex = "0" aria-label = "Toggle Primary Menu" role = "button"  onClick = {() => document.querySelector('#sidebar')
-                    .classList.toggle('sidebar-move-in')}>
-                <svg className = "hamburger-icon" xmlns = "http://www.w3.org/2000/svg" viewBox = "0 0 24 24">
-                    <path d = "M2 6h20v3H2zm0 5h20v3H2zm0 5h20v3H2z"/>
-                </svg>
-            </a>
+<div className = "wrapper">
+            <div id = "sidebar"
+              className = "sidebar"
+              role = "search" >
+                <a className = "hamburger-big-menu"
+                   tabIndex = "0"
+                   aria-label = "Toggle Primary Menu"
+                   role = "button"
+                   onClick = {() => {document.querySelector('#sidebar').classList.toggle('sidebar-move-in')
+                      this.props.updateQuery('')
+                      this.props.setFalse();
+                      document.querySelector('.info-widow').classList.remove('info-widow-move-in')}}>
+                        <svg className = "hamburger-icon"
+                             xmlns = "http://www.w3.org/2000/svg"
+                             viewBox = "0 0 24 24">
+                                <path d = "M2 6h20v3H2zm0 5h20v3H2zm0 5h20v3H2z"/>
+                        </svg>
+                  </a>
 
                 <input className ="search-input"
                     type = "text"
@@ -62,19 +59,33 @@ class MenuComponent extends Component {
                     aria-label = "Search location by typing"
                     tabIndex = "0"
                     value = { this.props.query }
-                    onChange = {event => this.searcher(event.target.value) }
+                    onChange = {event => this.unifier(event.target.value) }
+
                 />
                 <ul className = "locations-list" > {
-                    this.showSearch.map((sear, index) =>
-                    (<li key = {sear.id}
+                    this.props.searched.map((sear, index) =>
+                    (<li key = {index}
+                         id = {sear.id}
                          onClick = {this.props.onItemClick}
                          tabIndex = "0">
-                      {sear.title}
+                         {sear.title}
 
                      </li>))
                     }
                 </ul>
+              </div>
+
+                {
+                  <InfoWindowComponent
+                    venueInfo = {this.props.venueInfo}
+                    needOpen = {needOpen}
+                  />
+
+                }
+
             </div>
+
+
         );
 
     }
