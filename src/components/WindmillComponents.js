f9ab7b77b4ba1d22c0dd934b4f542415105ae12c
birozso <birozso@gmail.com>
2018-08-25 20:05:50 +0200

    Added state and cond. check for animation feature of markers.

--- src/components/WindmillComponents.js
+++ src/components/WindmillComponents.js
@@ -4,13 +4,46 @@ import PropTypes from 'prop-types'
 
 import {
   windmillStyle,
-  windmillCircleStyle, windmillCircleStyleHover,
+  windmillCircleStyle,
   windmillStickStyle, windmillStickStyleHover } from './WindmillStyle.js';
 import szelmalom from '../szelmalom.gif'
 
   // MyMapComponent passes values for creation , App receives click information and handles that.
 
 class WindmillComponent extends Component {
+
+  componentDidMount () {
+    this.loads();
+  }
+
+  // convert the object and loading the marker style to state from file/obj
+  loads = () => {
+    let windmillStyler = JSON.parse(JSON.stringify(windmillCircleStyle));
+    this.setState({ windmillStyler: windmillStyler })
+
+  }
+
+  state = {
+
+    windmillCircleStyleHover : {
+      position: 'absolute',
+      left: 50,
+      top: 10,
+      width: 90,
+      height: 90,
+      textAlign: 'center',
+      color: 'black',
+      fontSize: 16,
+      fontWeight: 'bold',
+      padding: 0,
+      cursor: 'pointer',
+      border: '3px solid black',
+      borderRadius: '100px',
+      backgroundColor: 'lightgray',
+      opacity: 0.95,
+    
+    },
+  }
 
   static propTypes = {
     // GoogleMap pass $hover props to hovered components
@@ -22,10 +55,12 @@ class WindmillComponent extends Component {
     location: PropTypes.object,
 
   };
+  
 
   static defaultProps = {};
 
   shouldComponentUpdate = shouldPureComponentUpdate;
+
 
   render() {
 
@@ -33,14 +68,111 @@ class WindmillComponent extends Component {
     const style = {
       ...windmillStyle,
       zIndex: $hover ? 500 : zIndex
+  
     };
+
+ 
+    
+
     // there is a possibility to use stick as a pointer - plan to later use
-    const circleStyle = $hover ? windmillCircleStyleHover : windmillCircleStyle;
+    const circleStyle = $hover ? this.state.windmillCircleStyleHover : this.state.windmillStyler;
     const stickStyle = $hover ? windmillStickStyleHover : windmillStickStyle;
+
+
+    /* if there is a click on a marker , clicked markers get a background color , if the previous state was a clicked one
+    *  it set back for the original shape . Check method uses prevState variable.
+    */
 
     return (
 
-       <div style={style} onClick = {() => this.props.onWindmillClick(id)} value = {id}>
+
+
+       <div style={style} onClick = {() => {this.props.onWindmillClick(id);
+         this.setState((prevState) => {
+            if (prevState.windmillStyler.backgroundColor === 'lightblue'){ 
+              return {windmillStyler: 
+                {position: 'absolute',
+                left: 50,
+                top: 10,
+                width: 90,
+                height: 90,
+                textAlign: 'center',
+                color: 'white',
+                fontSize: 0,
+                fontWeight: 'bold',
+                padding: 0,
+                cursor: 'pointer',
+                opacity: 0.75,
+                border: 'none',
+
+                }
+              }
+            }
+            else  return {windmillStyler: 
+                {border : '3px solid blue',
+                position: 'relative',
+                width: 90,
+                height: 90,
+                left: 50,
+                top: 10,
+                textAlign: 'center',
+                color: 'darkblue',
+                fontSize: 16,
+                fontWeight: 'bold',
+                padding: 0,
+                cursor: 'pointer',
+                opacity: 0.75,
+                backgroundColor: 'lightblue',
+
+                }
+              }
+            }
+          );
+          this.setState((prevState) => {
+            if (prevState.windmillCircleStyleHover.backgroundColor === 'lightblue'){ 
+              return {windmillCircleStyleHover: 
+                {position: 'absolute',
+                left: 50,
+                top: 10,
+                width: 90,
+                height: 90,
+                textAlign: 'center',
+                color: 'black',
+                fontSize: 16,
+                fontWeight: 'bold',
+                padding: 0,
+                cursor: 'pointer',
+                border: '3px solid black',
+                borderRadius: '100px',
+                backgroundColor: 'lightgray',
+                opacity: 0.95,
+
+                }
+              }
+            }
+            else  return {windmillCircleStyleHover: 
+                {position: 'absolute',
+                left: 50,
+                top: 10,
+                width: 90,
+                height: 90,
+                textAlign: 'center',
+                color: 'darkblue',
+                fontSize: 16,
+                fontWeight: 'bold',
+                padding: 0,
+                cursor: 'pointer',
+                border: '3px solid blue',
+                backgroundColor: 'lightblue',
+                opacity: 0.95,
+                borderRadius: 0,
+
+                }
+              }
+            }
+          );
+          }
+        } value = {id}>
           <div style={circleStyle} >
             <img width="100" src={szelmalom}
               alt={`Marker of windmill of  ${title}`}
